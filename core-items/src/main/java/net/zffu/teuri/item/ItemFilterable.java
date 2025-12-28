230f800209bd9919dc16353e996a600311fd7854
Zffu <103074097+Zffu@users.noreply.github.com>
2025-12-28 09:35:45 +0100

    feat: added ItemFilterable

---
+++ core-items/src/main/java/net/zffu/teuri/item/ItemFilterable.java
@@ -0,0 +1,19 @@
+package net.zffu.teuri.item;
+
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents any kind of Item that can be filtered.
+ * @since 1.0.0
+ */
+public interface ItemFilterable {
+
+    /**
+     * Determines whether an item matches the filter.
+     * @param stack the item stack.
+     * @return true if the item passed the filter, false if it didn't
+     */
+    boolean matches(@NotNull ItemStack stack);
+
+}
